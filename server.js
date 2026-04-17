a44e34f525f6d0c1cf04bfbd3f5938e95502647b
SHIVA VISHWAKARMA <raw66010@gmail.com>
2026-04-17 18:27:03 +0530

    cors done

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:3000", "http://127.0.0.1:5173", "http://127.0.0.1:5174" , "https://huma-website-opal.vercel.app"],
+  origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:3000", "http://127.0.0.1:5173", "http://127.0.0.1:5174" , "https://huma-website-opal.vercel.app" , "https://huma-admin-panel.vercel.app"],
   credentials: true
 }));
 app.use(express.json());
